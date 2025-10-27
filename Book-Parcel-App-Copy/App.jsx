d9ce3037060624be317b267cbd62df9bf1fa5472
Krishna-Kr-Yadav <krish.yad2005@gmail.com>
2025-10-28 02:01:01 +0530

    Completed the parcel react app

--- Book-Parcel-App-Copy/App.jsx
+++ Book-Parcel-App-Copy/App.jsx
@@ -1,11 +1,25 @@
 import Book from './Book'
 
+// function App(){
+//     return (
+//         <div className='container'>
+//             <Book image="https://tse2.mm.bing.net/th/id/OIP.C0gdVkwW-wUgvtPKzNFkrAHaJt?pid=Api&P=0&h=180" name="Java" price="200"/>
+//             <Book image="https://tse2.mm.bing.net/th/id/OIP.RM5JkPJFCnEs3bCJnjK7dwAAAA?pid=Api&P=0&h=180" name="React" price="500"/>
+//             <Book image="https://tse2.mm.bing.net/th/id/OIP.1QXmRpVYG0NA-fsZYrIrLwAAAA?pid=Api&P=0&h=180" name="DBMS" price="600"/>
+//         </div>
+//     )
+// }
+
 function App(){
-    return (
+    const Books=[{image:"https://tse2.mm.bing.net/th/id/OIP.C0gdVkwW-wUgvtPKzNFkrAHaJt?pid=Api&P=0&h=180" ,name:"Java", price:"200"},
+        {image:"https://tse2.mm.bing.net/th/id/OIP.RM5JkPJFCnEs3bCJnjK7dwAAAA?pid=Api&P=0&h=180" ,name:"React", price:"500"},
+        {image:"https://tse2.mm.bing.net/th/id/OIP.1QXmRpVYG0NA-fsZYrIrLwAAAA?pid=Api&P=0&h=180" ,name:"DBMS", price:"600"}
+    ]
+    return(
         <div className='container'>
-            <Book image="https://tse2.mm.bing.net/th/id/OIP.C0gdVkwW-wUgvtPKzNFkrAHaJt?pid=Api&P=0&h=180" name="Java" price="200"/>
-            <Book image="https://tse2.mm.bing.net/th/id/OIP.RM5JkPJFCnEs3bCJnjK7dwAAAA?pid=Api&P=0&h=180" name="React" price="500"/>
-            <Book image="https://tse2.mm.bing.net/th/id/OIP.1QXmRpVYG0NA-fsZYrIrLwAAAA?pid=Api&P=0&h=180" name="DBMS" price="600"/>
+            {Books.map((b,i)=>(
+                <Book key={i} book={b}/>
+            ))}
         </div>
     )
 }
--- Book-Parcel-App-Copy/Book.jsx
+++ Book-Parcel-App-Copy/Book.jsx
@@ -1,10 +1,21 @@
 import './Book.css'
-function Book({image,name,price}){
+// function Book({image,name,price}){
+//     return(
+//         <div className='card'>
+//             <img src={image} height="300" width="400" alt="book-img"/>
+//             <h3>Title: {name}</h3>
+//             <h4>Price: ₹{price}</h4>
+//             <button>Add to Cart</button>
+//         </div>
+//     )
+// }
+
+function Book({book}){
     return(
         <div className='card'>
-            <img src={image} height="300" width="400" alt="book-img"/>
-            <h3>Title: {name}</h3>
-            <h4>Price: ₹{price}</h4>
+            <img src={book.image} height="300" width="400" alt='book-img'/>
+            <h3>Title: {book.title}</h3>
+            <h4>Price: {book.price}</h4>
             <button>Add to Cart</button>
         </div>
     )
